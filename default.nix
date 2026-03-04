d58465746f5baccf22d57ee6ab9be23d0431c523
kent27x <kent27x@outlook.com>
2026-03-04 16:50:54 +0800

    Create default.nix

---
+++ default.nix
@@ -0,0 +1,41 @@
+{ pkgs, ... }: {
+
+  channel = "stable-24.11";
+
+  packages = [
+    pkgs.qemu
+    pkgs.wget
+    pkgs.htop
+  ];
+
+  idx.workspace.onStart = {
+    startvm = ''
+      set -e
+
+      VM_DIR="$HOME/qemu"
+      DISK="$VM_DIR/windows.qcow2"
+      mkdir -p "$VM_DIR"
+
+      # Create 11GB disk if missing
+      if [ ! -f "$DISK" ]; then
+        qemu-img create -f qcow2 "$DISK" 11G
+      fi
+
+      echo "Starting Windows VM..."
+
+      nohup qemu-system-x86_64 \
+        -enable-kvm \
+        -m 11264 \
+        -smp 4 \
+        -drive file="$DISK",format=qcow2 \
+        -vnc :0,password \
+        > /tmp/qemu.log 2>&1 &
+
+      # Keep workspace alive 12 hours
+      for i in {1..720}; do
+        echo "Running minute $i"
+        sleep 60
+      done
+    '';
+  };
+}
