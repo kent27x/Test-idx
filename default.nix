fa00e6d38953690fbafb6a3a57e8bda70cd296c3
kent27x <kent27x@outlook.com>
2026-03-04 17:23:44 +0800

    Update default.nix

--- default.nix
+++ default.nix
@@ -23,13 +23,16 @@
 
       echo "Starting Windows VM..."
 
+      # Start QEMU with VNC, log VNC info to file for RealVNC
       nohup qemu-system-x86_64 \
         -enable-kvm \
         -m 11264 \
         -smp 4 \
         -drive file="$DISK",format=qcow2 \
         -vnc :0,password \
-        > /tmp/qemu.log 2>&1 &
+        > /home/user/vnc-info.txt 2>&1 &
+
+      echo "VNC info saved to /home/user/vnc-info.txt"
 
       # Keep workspace alive 12 hours
       for i in {1..720}; do
@@ -37,5 +40,22 @@
         sleep 60
       done
     '';
+  };
+
+  idx.previews = {
+    enable = true;
+    previews = {
+      qemu = {
+        manager = "web";
+        command = [
+          "bash" "-lc"
+          "echo 'noVNC running on port 8888'"
+        ];
+      };
+      terminal = {
+        manager = "web";
+        command = [ "bash" ];
+      };
+    };
   };
 }
